022b24fd54f94a27d72472fac3839188badcd506
Yaroslav Terekh <yarolslavterekh@gmail.com>
2023-03-18 15:57:27 +0200

    Created upload page

--- BlobTask.App/src/app/app.component.ts
+++ BlobTask.App/src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UploadRequest } from './Interfaces/UploadRequest';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  @ViewChild('fileInput') uploadInput!: ElementRef;
+  public uploadGroup!: FormGroup;
   title = 'BlobTask.App';
+  selectedFile!: File;
+  fileInvalid!: boolean;
+  get email(): any {
+    return this.uploadGroup.get('email')
+  }
+
+  constructor(
+    private readonly fb: FormBuilder
+  ) {}
+
+  ngOnInit(): void {
+    this.uploadGroup = this.fb.group({
+      email: this.fb.control('', [Validators.required, Validators.email])
+    });
+  }
+
+  checkFile(): boolean {
+    if(this.selectedFile)
+      return this.selectedFile.name.split('.').pop() == 'docx';
+    
+    return false;
+  }
+
+  onFileSelected(): void {
+    this.selectedFile = this.uploadInput.nativeElement.files[0];
+    this.fileInvalid = this.checkFile(); 
+  }
+
+  onSubmit(): void {
+    this.fileInvalid = this.checkFile(); 
+
+    if(this.fileInvalid) {
+      let request: UploadRequest = {
+        email: this.uploadGroup.get('email')?.value,
+        file: this.selectedFile
+      };
+
+      console.log(request);
+    }
+
+       
+  }
 }
