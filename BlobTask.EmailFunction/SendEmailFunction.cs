b57eb369f1cff87922de57d74842b9352665d7ec
Yaroslav Terekh <yarolslavterekh@gmail.com>
2023-03-20 13:25:58 +0200

    Added ability to download file

--- BlobTask.EmailFunction/SendEmailFunction.cs
+++ BlobTask.EmailFunction/SendEmailFunction.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.Net;
 using System.Net.Mail;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.Sas;
+using BlobTask.EmailFunction.Models;
+using BlobTask.EmailFunction.Services.Abstractions;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Blob;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
 namespace BlobTask.EmailFunction
@@ -14,6 +20,13 @@ namespace BlobTask.EmailFunction
     [StorageAccount("BlobConnectionString")]
     public class SendEmailFunction
     {
+        private readonly IEmailSender _emailSender;
+
+        public SendEmailFunction(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
         [FunctionName("SendEmailFunction")]
         public void Run(
             [BlobTrigger("testcontainer/{name}")] Stream myBlob, 
@@ -21,44 +34,31 @@ namespace BlobTask.EmailFunction
             ILogger log
         )
         {
-            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=blobstoragetesttask;AccountKey=HPD7XY0yhFiT3jnCwIzx+7UH3Yj3QIm0KU3L3lvWo56MAlEWXuiL+18jvXfreDgD6C/Jy0PGast9+AStS5RDAA==;EndpointSuffix=core.windows.net");
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(Environment.GetEnvironmentVariable("BlobConnectionString"));
             CloudBlobClient blobClient = storageAccount.CreateCloudBlobClient();
             CloudBlobContainer container = blobClient.GetContainerReference("testcontainer");
             CloudBlockBlob blockBlob = container.GetBlockBlobReference(name);
             blockBlob.FetchAttributes();
 
             string emailAddress = blockBlob.Metadata["email"];
-            if (string.IsNullOrEmpty(emailAddress))
-            {
-                log.LogWarning($"Email address is not provided for blob '{name}'. Email cannot be sent.");
-                return;
-            }
 
-            var fromAddress = new MailAddress("yaroslav.terekh.reenbit@gmail.com", "Azure notification");
-            var toAddress = new MailAddress(emailAddress, "Recipient");
-            const string fromPassword = "esysvhbumcoapama";
-            const string subject = "New blob uploaded";
-            string body = $"<strong>A new file {name} has been successfully uploaded to the Azure Blob Storage!</strong>";
-
-            var smtp = new SmtpClient
+            var blobServiceClient = new BlobServiceClient(Environment.GetEnvironmentVariable("BlobConnectionString"));
+            var containerClient = blobServiceClient.GetBlobContainerClient("testcontainer");
+            var blobClient1 = containerClient.GetBlockBlobClient(name);
+            BlobSasBuilder sasBuilder = new BlobSasBuilder()
             {
-                Host = "smtp.gmail.com",
-                Port = 587,
-                EnableSsl = true,
-                DeliveryMethod = SmtpDeliveryMethod.Network,
-                Credentials = new NetworkCredential(fromAddress.Address, fromPassword),
-                Timeout = 20000
+                BlobContainerName = "testcontainer",
+                BlobName = name,
+                Resource = "b",
+                StartsOn = DateTimeOffset.UtcNow.AddMinutes(-5),
+                ExpiresOn = DateTimeOffset.UtcNow.AddMinutes(5),
+                Protocol = SasProtocol.Https,
             };
+            sasBuilder.SetPermissions(BlobSasPermissions.Read);
+            var url = blobClient1.GenerateSasUri(sasBuilder);
 
-            using (var message = new MailMessage(fromAddress, toAddress)
-            {
-                Subject = subject,
-                Body = body,
-                IsBodyHtml = true
-            })
-            {
-                smtp.Send(message);
-            }
+            var message = new Message(emailAddress, "Blob was uploaded", $"Your file <a href={url.OriginalString}>{name}</a> was uploaded");
+            _emailSender.SendEmail(message, "Azure notification");
         }
     }
 }
